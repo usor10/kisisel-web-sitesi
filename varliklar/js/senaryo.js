17625fb0b3ecc87179d83f6868a0577e0671b7a3
qu-4 <135950003+qu-4@users.noreply.github.com>
2023-07-12 03:21:07 +0300

    Update senaryo.js

--- varliklar/js/senaryo.js
+++ varliklar/js/senaryo.js
@@ -1,8 +1,8 @@
 window.addEventListener('load', function() {
-    var contentDiv = document.getElementById('content');
-    contentDiv.classList.add('dark');
+    var body = document.querySelector('body');
+    body.classList.add('dark');
 
     setTimeout(function() {
-        contentDiv.classList.remove('dark');
+        body.classList.remove('dark');
     }, 2000); // 2000 milisaniye (2 saniye) sonra aydınlanır
 });
