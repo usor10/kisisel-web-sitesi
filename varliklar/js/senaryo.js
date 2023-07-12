b51836f39e1921b314b76894af7b705a642a82ec
qu-4 <135950003+qu-4@users.noreply.github.com>
2023-07-12 03:33:21 +0300

    d

--- varliklar/js/senaryo.js
+++ varliklar/js/senaryo.js
@@ -10,8 +10,7 @@ document.onkeydown = function(e) {
 
 document.onkeydown = function(e) {
     if (e.key === "q" || e.key === "Q") {
-        var modelWindow = window.open("", "Model Penceresi", "width=400, height=300, top=100, left=100");
-        modelWindow.document.write("<h1>Model Penceresi</h1>");
-        modelWindow.document.write("<p>Bu küçük pencere model gibi çalışıyor!</p>");
+      var modelEkran = document.getElementById("modelEkran");
+      modelEkran.style.display = "flex";
     }
-};
+  };
