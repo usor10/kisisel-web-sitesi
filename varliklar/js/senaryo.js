203bcecee3f1f38d3cd5cbd2249388bf80807b5f
qu-4 <135950003+qu-4@users.noreply.github.com>
2023-07-12 03:23:22 +0300

    d

--- varliklar/js/senaryo.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('load', function() {
-    var body = document.querySelector('body');
-    body.classList.add('dark');
-
-    setTimeout(function() {
-        body.classList.remove('dark');
-    }, 2000); // 2000 milisaniye (2 saniye) sonra aydınlanır
-});
