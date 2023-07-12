101f1ae8d90fac9f18dc145c10e0bee3d7d18795
qu-4 <135950003+qu-4@users.noreply.github.com>
2023-07-12 03:55:31 +0300

    d

--- varliklar/js/senaryo.js
+++ varliklar/js/senaryo.js
@@ -1,13 +1,3 @@
-document.onkeydown = function(e) {
-    if (e.keyCode == 116) {
-        var yenilendiDiv = document.createElement("div");
-        yenilendiDiv.innerHTML = "Yenilendi!";
-        yenilendiDiv.style.textAlign = "center";
-        yenilendiDiv.style.fontSize = "24px";
-        document.body.appendChild(yenilendiDiv);
-    }
-};
-
 var modelEkranAcik = false;
 
 document.onkeydown = function(e) {
