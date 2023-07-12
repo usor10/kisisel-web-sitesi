d6f0ea8f4f1da2a35d7a4959f5da60a0a7888b6c
qu-4 <135950003+qu-4@users.noreply.github.com>
2023-07-12 03:28:01 +0300

    Update senaryo.js

--- varliklar/js/senaryo.js
+++ varliklar/js/senaryo.js
@@ -1,7 +1,9 @@
-window.onload = function() {
-    var yenilendiDiv = document.createElement("div");
-    yenilendiDiv.innerHTML = "Yenilendi!";
-    yenilendiDiv.style.textAlign = "center";
-    yenilendiDiv.style.fontSize = "24px";
-    document.body.appendChild(yenilendiDiv);
+document.onkeydown = function(e) {
+    if (e.keyCode == 116) {
+        var yenilendiDiv = document.createElement("div");
+        yenilendiDiv.innerHTML = "Yenilendi!";
+        yenilendiDiv.style.textAlign = "center";
+        yenilendiDiv.style.fontSize = "24px";
+        document.body.appendChild(yenilendiDiv);
+    }
 };
